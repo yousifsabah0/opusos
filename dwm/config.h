4490cd472d571ba317c5503c1dd1f44999379ebe
Yousif Sabah <iyousifsabah@gmail.com>
2024-03-06 20:22:20 +0300

    dwm: apply 'barpadding' patch

--- dwm/config.h
+++ dwm/config.h
@@ -5,6 +5,9 @@ static const unsigned int snap      = 35;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
+static const int vertpad            = 15;       /* vertical padding of bar */
+static const int sidepad            = 15;       /* horizontal padding of bar */ 
+
 static const char *fonts[]          = { "monospace:size=10" };
 
 static const char col_gray1[]       = "#222222";
