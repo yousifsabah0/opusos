820785897b808af5bb76b7bc8a903e1dd66bb652
Yousif Sabah <iyousifsabah@gmail.com>
2024-03-06 22:10:08 +0300

    st: fix font family name

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SF Pro:size=14";
+static char *font = "SF Mono:size=14";
 static int borderpx = 30;
 
 /*
