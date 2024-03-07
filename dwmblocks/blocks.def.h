c1a2d48dcc77a605c343458277150763f4b16ec7
Yousif Sabah <iyousifsabah@gmail.com>
2024-03-07 15:55:48 +0300

    configs/picom: decrease border radius

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date +'%a %b %d %l:%M %p'",					5,		0},
+	{"", "date +'%a  %b  %d %l:%M %p'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
