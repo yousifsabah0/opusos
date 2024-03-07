656288eff928262661ed7f1d0b838fbea2bd4825
Yousif Sabah <iyousifsabah@gmail.com>
2024-03-07 14:54:20 +0300

    st: make font size smaller

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = ".:size=14";
+static char *font = ".:size=12";
 static int borderpx = 30;
 
 /*
